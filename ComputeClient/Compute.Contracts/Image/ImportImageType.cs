a10e59ea9b5a3e11da3e5280b92b97ca4ae7d334
Hamed Soleymani <hamed.soleymani@itaas.dimensiondata.com>
2016-10-24 16:01:55 +1100

    Update the import image type contract

--- ComputeClient/Compute.Contracts/Image/ImportImageType.cs
+++ ComputeClient/Compute.Contracts/Image/ImportImageType.cs
@@ -9,31 +9,73 @@
     [System.Xml.Serialization.XmlRootAttribute("importImage", Namespace = "urn:didata.com:api:cloud:types", IsNullable = false)]
     public partial class ImportImageType
     {
-        /// <remarks/>
-        public string ovfPackage;
+        private string ovfPackageField;
+
+        private string nameField;
+
+        private string descriptionField;
+
+        private bool guestOsCustomizationField;
+
+        private bool guestOsCustomizationSpecifiedField;
+
+        private string ItemField;
+
+        private ItemChoiceType ItemElementNameField;
 
         /// <remarks/>
-        public string name;
+        public string ovfPackage
+        {
+            get { return this.ovfPackageField; }
+            set { this.ovfPackageField = value; }
+        }
 
         /// <remarks/>
-        public string description;
+        public string name
+        {
+            get { return this.nameField; }
+            set { this.nameField = value; }
+        }
+
+        /// <remarks/>
+        public string description
+        {
+            get { return this.descriptionField; }
+            set { this.descriptionField = value; }
+        }
+
+        /// <remarks/>
+        public bool guestOsCustomization
+        {
+            get { return this.guestOsCustomizationField; }
+            set { this.guestOsCustomizationField = value; }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool guestOsCustomizationSpecified
+        {
+            get { return this.guestOsCustomizationSpecifiedField; }
+            set { this.guestOsCustomizationSpecifiedField = value; }
+        }
 
         /// <remarks/>
         [System.Xml.Serialization.XmlElementAttribute("clusterId", typeof(string))]
         [System.Xml.Serialization.XmlElementAttribute("datacenterId", typeof(string))]
         [System.Xml.Serialization.XmlChoiceIdentifierAttribute("ItemElementName")]
-        public string Item;
+        public string Item
+        {
+            get { return this.ItemField; }
+            set { this.ItemField = value; }
+        }
 
         /// <remarks/>
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public ItemChoiceType ItemElementName;
-
-        /// <remarks/>
-        public bool guestOsCustomization;
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public bool guestOsCustomizationSpecified;
+        public ItemChoiceType ItemElementName
+        {
+            get { return this.ItemElementNameField; }
+            set { this.ItemElementNameField = value; }
+        }
     }
 
     /// <remarks/>
