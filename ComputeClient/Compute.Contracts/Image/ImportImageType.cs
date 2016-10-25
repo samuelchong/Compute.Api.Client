dd4b116e72bd26e82d3aaf5ae732b6f685f7bf1e
Hamed Soleymani <hamed.soleymani@itaas.dimensiondata.com>
2016-10-25 12:06:57 +1100

    Fixed the import image contract

--- ComputeClient/Compute.Contracts/Image/ImportImageType.cs
+++ ComputeClient/Compute.Contracts/Image/ImportImageType.cs
@@ -9,73 +9,32 @@
     [System.Xml.Serialization.XmlRootAttribute("importImage", Namespace = "urn:didata.com:api:cloud:types", IsNullable = false)]
     public partial class ImportImageType
     {
-        private string ovfPackageField;
-
-        private string nameField;
-
-        private string descriptionField;
-
-        private bool guestOsCustomizationField;
-
-        private bool guestOsCustomizationSpecifiedField;
-
-        private string ItemField;
-
-        private ItemChoiceType ItemElementNameField;
 
         /// <remarks/>
-        public string ovfPackage
-        {
-            get { return this.ovfPackageField; }
-            set { this.ovfPackageField = value; }
-        }
+        public string ovfPackage;
 
         /// <remarks/>
-        public string name
-        {
-            get { return this.nameField; }
-            set { this.nameField = value; }
-        }
+        public string name;
 
         /// <remarks/>
-        public string description
-        {
-            get { return this.descriptionField; }
-            set { this.descriptionField = value; }
-        }
-
-        /// <remarks/>
-        public bool guestOsCustomization
-        {
-            get { return this.guestOsCustomizationField; }
-            set { this.guestOsCustomizationField = value; }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public bool guestOsCustomizationSpecified
-        {
-            get { return this.guestOsCustomizationSpecifiedField; }
-            set { this.guestOsCustomizationSpecifiedField = value; }
-        }
+        public string description;
 
         /// <remarks/>
         [System.Xml.Serialization.XmlElementAttribute("clusterId", typeof(string))]
         [System.Xml.Serialization.XmlElementAttribute("datacenterId", typeof(string))]
         [System.Xml.Serialization.XmlChoiceIdentifierAttribute("ItemElementName")]
-        public string Item
-        {
-            get { return this.ItemField; }
-            set { this.ItemField = value; }
-        }
+        public string Item;
 
         /// <remarks/>
         [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public ItemChoiceType ItemElementName
-        {
-            get { return this.ItemElementNameField; }
-            set { this.ItemElementNameField = value; }
-        }
+        public ItemChoiceType ItemElementName;
+
+        /// <remarks/>
+        public bool guestOsCustomization;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool guestOsCustomizationSpecified;
     }
 
     /// <remarks/>
@@ -84,6 +43,7 @@
     [System.Xml.Serialization.XmlTypeAttribute(Namespace = "urn:didata.com:api:cloud:types", IncludeInSchema = false)]
     public enum ItemChoiceType
     {
+
         /// <remarks/>
         clusterId,
 
